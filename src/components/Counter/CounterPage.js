ce1b478016fe2f015ce8bc4b3f181171107bd9ec
a805431 <gergana.ivanova@yara.com>
2023-03-20 15:35:47 +0200

    s18v284

--- src/components/Counter/CounterPage.js
+++ src/components/Counter/CounterPage.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -10,19 +11,41 @@ function CounterPage({ initialCount }) {
 
   const decrement = () => {
     setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    //if we get back NaN for event.target.value, which is a falsy value
+    //we will assign 0 instead to the value variable
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
   }
 
   return (
-    <div style={{margin: "0.75rem"}}> 
-      <h1 style={{fontSize: "1.125rem", lineHeight: "1.75rem" }}>Count is {count}</h1>
+    <div style={{ margin: '0.75rem' }}>
+      <h1 style={{ fontSize: '1.125rem', lineHeight: '1.75rem' }}>
+        Count is {count}
+      </h1>
       <ButtonWrapper>
         <IncrementButton onClick={increment}>Increment</IncrementButton>
         <IncrementButton onClick={decrement}>Decrement</IncrementButton>
       </ButtonWrapper>
-    
-      <form>
+
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
-        <NumberInput type="number"/>
+        {/*value={valueToAdd || ''} --> means if we have valueToAdd piece of state of 0, then don't print out the zero, 
+        just put in empty string instead*/}
+        <NumberInput
+          type="number"
+          value={valueToAdd || ''}
+          onChange={handleChange}
+        />
         <IncrementButton>Add it!</IncrementButton>
       </form>
     </div>

f0465ece1399600359e3628eff21d4d50f53003c
a805431 <gergana.ivanova@yara.com>
2023-03-20 15:01:40 +0200

    s18v283

--- src/components/Counter/CounterPage.js
+++ src/components/Counter/CounterPage.js
@@ -1,13 +1,30 @@
-import { IncrementButton } from './counterPage.css';
-import useCounterAndLog from '../../hooks/use-counter-and-log';
+import { IncrementButton, ButtonWrapper, NumberInput } from './counterPage.css';
+import { useState } from 'react';
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounterAndLog(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <IncrementButton onClick={increment}>Increment</IncrementButton>
+    <div style={{margin: "0.75rem"}}> 
+      <h1 style={{fontSize: "1.125rem", lineHeight: "1.75rem" }}>Count is {count}</h1>
+      <ButtonWrapper>
+        <IncrementButton onClick={increment}>Increment</IncrementButton>
+        <IncrementButton onClick={decrement}>Decrement</IncrementButton>
+      </ButtonWrapper>
+    
+      <form>
+        <label>Add a lot!</label>
+        <NumberInput type="number"/>
+        <IncrementButton>Add it!</IncrementButton>
+      </form>
     </div>
   );
 }
--- src/components/Counter/counterPage.css.js
+++ src/components/Counter/counterPage.css.js
@@ -3,7 +3,20 @@ import styled from 'styled-components';
 export const IncrementButton = styled.button`
   height: 40px;
   width: 120px;
-  background-color: rgb(64,178,214,255);
-  border-color: rgb(27,142,186,255);
+  background-color: rgb(64, 178, 214, 255);
+  border-color: rgb(27, 142, 186, 255);
   font-weight: bold;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const NumberInput = styled.input`
+  padding: 0.25rem;
+  margin: 0.75rem;
+  background-color: #f9fafb;
+  border-width: 1px;
+  border-color: #d1d5db;
 `;
